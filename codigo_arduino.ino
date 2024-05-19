24bbdf755d4c87b60dd83b06a5b74e2c02a0e48c
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2024-05-19 19:05:44 -0300

    Create codigo_arduino.ino

---
+++ codigo_arduino.ino
@@ -0,0 +1,15 @@
+const int relePin = 7;  // Defina o pino onde o relé está conectado
+
+void setup() {
+  pinMode(relePin, OUTPUT);
+  digitalWrite(relePin, LOW);  // Inicialmente, o relé está desligado
+
+  // Se o Arduino está ligado, então liga o relé por 30 segundos
+  digitalWrite(relePin, HIGH);  // Liga o relé
+  delay(30000);                 // Mantém ligado por 30 segundos
+  digitalWrite(relePin, LOW);   // Desliga o relé
+}
+
+void loop() {
+  // Não precisa fazer nada no loop
+}
