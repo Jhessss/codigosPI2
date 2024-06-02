0242ba3d3401c0f97b1f35f82d137a0882af47bf
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2024-06-02 15:16:05 -0300

    Update led_control.py

--- led_control.py
+++ led_control.py
@@ -1,15 +1,4 @@
 """Executando o código
-sudo apt-get update
-sudo apt-get install python3-rpi.gpio
-
-Conexões físicas.
-GPIO 17 (Pino físico 11)
-GPIO 27 (Pino físico 13)
-GPIO 22 (Pino físico 15)
-GPIO 23 (Pino físico 16)
-GPIO 24 (Pino físico 18)
-
-
 python3 led_control.py
 
 """
@@ -18,8 +7,7 @@ import RPi.GPIO as GPIO
 import time
 
 # Definir os pinos GPIO
-led_pins = [17, 27, 22, 23, 24]
-
+led_pins = 17
 # Configurar o modo da numeração dos pinos
 GPIO.setmode(GPIO.BCM)
 
