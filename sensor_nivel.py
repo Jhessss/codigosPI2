cd0f0a6046e7cb604fcb05c36d91fd6f95df40f2
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2024-06-26 15:33:20 -0300

    Create sensor_nivel.py

---
+++ sensor_nivel.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configuração dos pinos
+sensor_pin = 17
+
+# Configuração do GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def sensor_callback(channel):
+    if GPIO.input(sensor_pin):
+        print("Nível da água baixo")
+        # Adicione aqui o código que deseja interromper quando o nível da água estiver baixo
+    else:
+        print("Nível da água suficiente para operação")
+
+# Configurar interrupção no GPIO
+GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=sensor_callback)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Encerrando o programa")
+finally:
+    GPIO.cleanup()
